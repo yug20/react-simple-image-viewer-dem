cf5e17ac951d4f9d0e7944f0412af0e8ab4b05ec
yugjadvani <110123287+yug20@users.noreply.github.com>
2022-12-08 10:59:23 +0530

    image viewer

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import React, { useState, useCallback } from "react";
+import { createRoot } from "react-dom/client";
+import ImageViewer from "react-simple-image-viewer";
+
+function App() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isViewerOpen, setIsViewerOpen] = useState(false);
+  const images = [
+    "http://placeimg.com/1200/800/nature",
+    "http://placeimg.com/800/1200/nature",
+    "http://placeimg.com/1920/1080/nature",
+    "http://placeimg.com/1500/500/nature"
+  ];
+
+  const openImageViewer = useCallback((index) => {
+    setCurrentImage(index);
+    setIsViewerOpen(true);
+  }, []);
+
+  const closeImageViewer = () => {
+    setCurrentImage(0);
+    setIsViewerOpen(false);
+  };
+
+  return (
+    <div>
+      {images.map((src, index) => (
+        <img
+          src={src}
+          onClick={() => openImageViewer(index)}
+          width="300"
+          key={index}
+          style={{ margin: "2px" }}
+          alt=""
+        />
+      ))}
+
+      {isViewerOpen && (
+        <ImageViewer
+          src={images}
+          currentIndex={currentImage}
+          onClose={closeImageViewer}
+          disableScroll={false}
+          backgroundStyle={{
+            backgroundColor: "rgba(0,0,0,0.9)"
+          }}
+          closeOnClickOutside={true}
+        />
+      )}
+    </div>
+  );
+}
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
